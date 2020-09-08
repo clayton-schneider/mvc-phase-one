d3166c296835f22b97a02c000e183ef484155150
Clayton <schneiderc2016@gmail.com>
2020-09-08 08:58:16 -0400

    1st draft

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,36 @@
 <template>
   <nav v-if="mode == 'home'" class="nav nav--home">
-    <h3 class="nav__logo">Media Voices For Children</h3>
+    <g-link to="/"><h3 class="nav__logo">Media Voices For Children</h3></g-link>
 
     <div class="nav__links">
-      <g-link to="/contact">Contact</g-link>
-      <a href="https://www.mediavoicesforchildren.org/donate">Donate</a>
-      <g-link to="/stream" class="orange">Watch Our Stream</g-link>
+      <g-link class="nav__link nav__link--light" to="/contact">Contact</g-link>
+      <a
+        class="nav__link nav__link--light"
+        href="https://www.mediavoicesforchildren.org/donate"
+        >Donate</a
+      >
+      <g-link to="/stream" class="nav__link nav__link--light orange"
+        >Watch Our Stream</g-link
+      >
+    </div>
+  </nav>
+  <nav v-else-if="mode == 'alt'" class="nav nav--alt">
+    <g-link to="/"
+      ><h3 class="nav__logo nav__logo--dark">
+        Media Voices For Children
+      </h3></g-link
+    >
+
+    <div class="nav__links">
+      <g-link class="nav__link nav__link--dark" to="/contact">Contact</g-link>
+      <a
+        class="nav__link nav__link--dark"
+        href="https://www.mediavoicesforchildren.org/donate"
+        >Donate</a
+      >
+      <g-link to="/stream" class="nav__link nav__link--dark"
+        >Watch Our Stream</g-link
+      >
     </div>
   </nav>
 </template>
--- src/pages/Stream.vue
+++ src/pages/Stream.vue
@@ -1,14 +1,76 @@
 <template>
-  <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-  </Layout>
+  <main>
+    <Nav mode="alt" />
+
+    <section class="stream">
+      <div class="stream__img"></div>
+      <p>Now Streaming: <span class="bold">Children Of Bal Ashram</span></p>
+      <p>Streaming Until: Sept. 14th 12:00pm</p>
+    </section>
+
+    <section class="offer">
+      <div class="offer__left">
+        <div class="offer__content">
+          <h2>Limited Time Offer</h2>
+          <p class="offer__exp">
+            Get the information and tools you need to advocate for children's
+            rights around the world and learn more about the young activists at
+            the forefront of today's struggle to end child labor and modern
+            slavery.
+          </p>
+          <div class="offer__benefits">
+            <div class="offer__benefit">
+              <g-image src="~/assets/imgs/check.svg" alt="benefit" />
+              <p>
+                Access our seven-part study guide, suitable for remote learning
+                for grades 7-12
+              </p>
+            </div>
+            <div class="offer__benefit">
+              <g-image src="~/assets/imgs/check.svg" alt="benefit" />
+              <p>
+                Learn more about youth activism and how you, your students and
+                community can make get involved
+              </p>
+            </div>
+            <div class="offer__benefit">
+              <g-image src="~/assets/imgs/check.svg" alt="benefit" />
+              <p>
+                Learn more about Media Voices for Children and receive our
+                quarterly magazine on all aspects of child rights
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="offer__signup">
+          <h3>Complete This Form And We Will Send You Your Study Guide</h3>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="email" placeholder="Email Address" />
+          <a href="" class="btn">Send Me The Study Guide!</a>
+        </div>
+      </div>
+      <g-image
+        src="~/assets/imgs/study-guide.jpg"
+        alt="Study Guide"
+        height="650"
+      />
+    </section>
+    <Footer />
+  </main>
 </template>
 
 <script>
+import Footer from '~/components/Footer';
+import Nav from '~/components/Nav';
 export default {
   metaInfo: {
-    title: 'About us'
-  }
-}
+    title: 'Stream Children Of Bal Ashram',
+  },
+  components: {
+    Nav,
+    Footer,
+  },
+};
 </script>
