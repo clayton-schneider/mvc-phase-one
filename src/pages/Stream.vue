78863190b59c7825e61899f5fa8d00fee5fcb502
Clayton <schneiderc2016@gmail.com>
2020-09-21 13:14:01 -0400

    End streaming ability

--- src/pages/Stream.vue
+++ src/pages/Stream.vue
@@ -3,71 +3,13 @@
     <Nav mode="alt" />
 
     <section class="stream">
-      <iframe
-        src="https://player.vimeo.com/video/304839811"
-        width="100%"
-        height="600"
-        frameborder="0"
-        allow="autoplay; fullscreen"
-        allowfullscreen
-      ></iframe>
-      <p>Now Streaming: <span class="bold">Children Of Bal Ashram</span></p>
-      <p>Streaming Until: Sept. 19th 5:00pm</p>
+      <div class="stream__img"></div>
     </section>
 
     <section class="offer">
-      <div class="offer__left">
-        <div class="offer__content">
-          <h2>Limited Time Offer</h2>
-          <p class="offer__exp">
-            Get the information and tools you need to advocate for children's
-            rights around the world and learn more about the young activists at
-            the forefront of today's struggle to end child labor and modern
-            slavery.
-          </p>
-          <div class="offer__benefits">
-            <div class="offer__benefit">
-              <g-image src="~/assets/imgs/check.svg" alt="benefit" />
-              <p>
-                Access our seven-part study guide, suitable for remote learning
-                for grades 7-12
-              </p>
-            </div>
-            <div class="offer__benefit">
-              <g-image src="~/assets/imgs/check.svg" alt="benefit" />
-              <p>
-                Learn more about youth activism and how you, your students and
-                community can get involved
-              </p>
-            </div>
-            <div class="offer__benefit">
-              <g-image src="~/assets/imgs/check.svg" alt="benefit" />
-              <p>
-                Learn more about Media Voices for Children and receive our
-                quarterly magazine on all aspects of child rights
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="offer__signup">
-          <h3>Complete This Form And We Will Send You Your Study Guide</h3>
-          <!-- <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="email" placeholder="Email Address" />
-          <a href="" class="btn">Send Me The Study Guide!</a> -->
-        </div>
-        <div
-          class="ml-form-embed"
-          data-account="2013992:s1b0g2g4q1"
-          data-form="2645135:z7r7e5"
-        ></div>
+      <div class="offer__content">
+        <h2>The time to stream Children of Bal Ashram has passed</h2>
       </div>
-      <g-image
-        src="~/assets/imgs/study-guide.jpg"
-        alt="Study Guide"
-        height="650"
-      />
     </section>
     <Footer />
   </main>
