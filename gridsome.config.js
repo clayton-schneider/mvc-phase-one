c161dcd019112cbd3fefabb36cdb4220d475a4ab
Clayton <schneiderc2016@gmail.com>
2020-11-11 11:27:09 -0500

    another test

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Learn Child Rights',
   plugins: [],
   chainWebpack: config => {
     config.resolve.alias.set('@images', '@/assets/imgs');
