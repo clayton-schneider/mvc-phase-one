10d350d656fad45ab46a6234bffe6338e3b58c01
Clayton Schneider <schneiderc2016@gmail.com>
2021-03-23 10:10:11 -0400

    Added fourth issue

--- src/pages/Read.vue
+++ src/pages/Read.vue
@@ -3,12 +3,11 @@
     <Nav mode="alt" />
     <section class="read">
       <h2 class="headline">Read our latest quarterly magazine</h2>
-      <iframe
-        allowfullscreen
-        allow="fullscreen"
-        style="border:none;width:100%;height:500px;"
-        src="//e.issuu.com/embed.html?d=volume_three_with_tisbury_changes&u=mediavoices"
-      ></iframe>
+      <iframe 
+        allowfullscreen="true" 
+        style="border:none;width:100%;height:500px;" 
+        src="//e.issuu.com/embed.html?d=mag_volume_four____&hideIssuuLogo=true&showOtherPublicationsAsSuggestions=true&u=mediavoices">
+      </iframe>
 
       <div class="archive">
         <div class="volume">
@@ -17,8 +16,17 @@
             allowfullscreen
             allow="fullscreen"
             style="border:none;width:100%;height:500px;"
-            src="//e.issuu.com/embed.html?d=volume_two_final-_for_print___with_qr_code_fixed_1&u=mediavoices"></iframe>;
+            src="//e.issuu.com/embed.html?d=volume_three_with_tisbury_changes&u=mediavoices"
           ></iframe>
+        </div>
+        <div class="volume">
+          <h3>Volume Two</h3>
+          <iframe
+            allowfullscreen
+            allow="fullscreen"
+            style="border:none;width:100%;height:500px;"
+            src="//e.issuu.com/embed.html?d=volume_two_final-_for_print___with_qr_code_fixed_1&u=mediavoices">
+          </iframe>;
         </div>
         <div class="volume">
           <h3>Volume One</h3>
@@ -26,8 +34,7 @@
             allowfullscreen
             allow="fullscreen"
             style="border:none;width:100%;height:500px;"
-            src="//e.issuu.com/embed.html?d=e-mail_mag_for_print_&u=mediavoices"></iframe>;
-          ></iframe>
+            src="//e.issuu.com/embed.html?d=e-mail_mag_for_print_&u=mediavoices"></iframe>
         </div>
       </div>
     </section>
